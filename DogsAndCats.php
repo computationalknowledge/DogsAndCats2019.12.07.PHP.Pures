72d16db26fc659eb95f86fc00723918109bcf498
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2019-12-07 09:54:22 -0500

    Update DogsAndCats.php

--- DogsAndCats.php
+++ DogsAndCats.php
@@ -1,6 +1,7 @@
 <?php
 $dogHouse = array();
 $catHouse = array();
+$breeds = array("frenchie", "alsanse", "atlantic", "swiss", "woodland" );
 class Dog
 {
     // Declare  properties
@@ -27,10 +28,12 @@ class Cat
 for ($counter=1; $counter <= 100; $counter++){
 $fido = new Dog(); 
 $fido->name = (string)$counter;
+$fido->breed = (string)$breeds[rand(0,4)];
 $dogHouse[] = $fido;
 
 $fifi = new Cat();
 $fifi->name = (string)$counter;
+$fifi->breed = (string)$breeds[rand(0,4)];
 $catHouse[] = $fifi;
 
 }
